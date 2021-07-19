9eb08220510858ef53f7e9a3afe6f9305cb1cb9a
Daniel Martorell <dani.martorell@gmail.com>
2021-07-19 21:19:43 +0200

    Add action.delete_task code

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,24 @@ function App() {
   const [taskName, setTaskName] = useState('');
   const ACTIONS = {
     ADD_TASK: 'add_task',
+    DELETE_TASK: 'delete_task',
   };
 
   const reducer = ((state, action) => {
     switch (action.type) {
       case ACTIONS.ADD_TASK:
         return [...state, { id: Date.now(), name: action.payload, complete: false }];
+      case ACTIONS.DELETE_TASK:
+        return state.filter((task) => task.id !== action.payload);
       default:
         return state;
     }
   });
   const [todos, dispatch] = useReducer(reducer, []);
 
+  const deleteTask = (id) => {
+    dispatch({ type: ACTIONS.DELETE_TASK, payload: id });
+  };
   const handleSubmit = (e) => {
     e.preventDefault();
     dispatch({ type: ACTIONS.ADD_TASK, payload: taskName });
@@ -33,7 +39,10 @@ function App() {
       <ul>
         {
         todos.map((task) => (
-          <li key={task.id}>{task.name}</li>
+          <li key={task.id}>
+            {task.name}
+            <button type="button" onClick={() => deleteTask(task.id)}>Delete</button>
+          </li>
         ))
       }
       </ul>
